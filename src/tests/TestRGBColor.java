581ef39a25070e3582c6be4727e25f3bf83abf0a
acch2016 <ie696846@iteso.mx>
2017-09-08 18:06:02 -0500

    170908_1805_c1objectcreated

--- src/tests/TestRGBColor.java
+++ src/tests/TestRGBColor.java
@@ -4,6 +4,10 @@ public class TestRGBColor {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+				RGBColor c1 = new RGBColor();
+//		c1.print();
+//		RGBColor c2 = new RGBColor()
 
 	}
 
