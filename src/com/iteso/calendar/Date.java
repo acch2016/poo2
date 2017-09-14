13bf5ec6afce4bc287ec1d54b7215f32881abc5d
Alejandro Canale <ie696846@iteso.mx>
2017-09-13 22:07:03 -0500

    reverted

--- src/com/iteso/calendar/Date.java
+++ src/com/iteso/calendar/Date.java
@@ -1,5 +1,5 @@
 package com.iteso.calendar;
-
+// quiero ver si funciona3
 public class Date {
 	
 	/* Attributes o instance variables */
@@ -7,7 +7,7 @@ public class Date {
 	private String monthName;
 	private int format = 0;
 	
-	/* Constructors */
+	/*Constructores*/
 	public Date(){
 	}
 	
